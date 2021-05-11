7eb689d40d64d3f0fa831d8e13dcd5f3853bd84b
Cosimo Conte <cosimocon@yahoo.it>
2021-05-11 17:09:06 +0200

    Better commenting

--- src/estimation/estimation/uwb_positioning.py
+++ src/estimation/estimation/uwb_positioning.py
@@ -11,13 +11,25 @@ import functions
 
 
 class UwbPositioning(Node):
-    """Node to estimate the position using uwb_positioning methods"""
+    """
+    Node to estimate the position using only the UWB
+
+    Params:
+      sensor_id (str): The anchor ID of the UWB sensor that need to be tracked
+        is the <anchor_id> param provided @ the UWB gazebo plugin
+
+      method (str): Can be "LS" or "GN":
+        "LS" is the Least-Square method
+        "GN" is for the Gauss-Newton method
+
+      iterations (int): Numbers of iterations, valid only with the GN method
+    """
 
     def __init__(self):
         super().__init__("uwb_positioning")
 
         self.anchors_ = {}
-        self.sensor_est_pos_ = [0.01, 0.01, 0.01]
+        self.sensor_est_pos_ = [0.01, 0.01, 0.01]  # To be randomized
 
         # Parameters declaration
         self.sensor_id_ = self.declare_parameter("sensor_id", "0")
@@ -32,30 +44,51 @@ class UwbPositioning(Node):
         self.iterations_ = self.get_parameter(
             "iterations").get_parameter_value().integer_value
 
-        # Setting up sensors subscribers
+        # Namespace check
+        if(self.get_namespace() == '/'):
+            self.get_logger().error("uwb_positioning need a namespace")
+            self.destroy_node()
+
+        # Setting up sensors subscriber for the UWB plugin
         self.sensor_subscriber_ = self.create_subscription(
             UwbSensor, "/uwb_sensor_" + self.sensor_id_, self.callback_sensor_subscriber, 10)
 
-        # Setting up a publishers to record the estimation error
+        # Setting up a publishers to send the estimated position
         self.estimator_topic_name_ = self.get_namespace() + "/estimated_pos"
         self.position_mse_publisher_ = self.create_publisher(
             PointStamped, self.estimator_topic_name_, 10)
 
-        self.get_logger().info("uwb_positioning has started")
+        self.get_logger().info(f"""
+                                uwb_positioning has started:
+                                  Sensor ID:  {self.sensor_id_}
+                                  Method:     {self.method_}
+                                  Iterations  {self.iterations_}
+                                """)
 
     def callback_sensor_subscriber(self, msg):
+        """
+        Receive a pseudorange from the UWB sensor
+
+        Args:
+            msg (UwbSensor): The message received by the plugin
+        """
+
+        # Saving the message in a dict
         self.anchors_[msg.anchor_id] = msg
 
+        # Only if trilateration is possible
         if len(self.anchors_) > 3:
             if(self.method_ == "LS"):
                 self.sensor_est_pos_ = functions.ls_trilateration(
                     self.anchors_)
 
             if(self.method_ == "GN"):
+                # Perform N iterations of GN
                 for _ in range(self.iterations_):
                     self.sensor_est_pos_ = functions.gauss_newton_trilateration(
                         self.sensor_est_pos_, self.anchors_)
 
+            # Sending the estimated position and the name of the node that generated it
             msg = PointStamped()
             msg.header.frame_id = self.estimator_topic_name_
             msg.point.x = self.sensor_est_pos_[0]
--- src/functions/functions/trilateration.py
+++ src/functions/functions/trilateration.py
@@ -2,6 +2,22 @@ import numpy as np
 
 
 def extract_anchor_data(anchors):
+    """Extract all the data from a dict of anchors
+
+    Args:
+        anchors (dict): Dict with keys the anchor Id and value the UwbSensor
+          message sent by that anchor, in this way only the last message is
+          saved
+
+    Returns:
+        anchor_pos (np.array): Matrix containing for each row the 3 coordinates
+          of one anchor
+
+        ranges (np.array): Array containing all the pseudoranges
+
+        N (int): Number of anchors
+    """
+
     N = len(anchors)
 
     # Extract anchor positions and ranges
@@ -19,6 +35,16 @@ def extract_anchor_data(anchors):
 
 
 def ls_trilateration(anchors):
+    """
+    Solve the Least-Square problem to estimate the position of the sensor
+      from the pseudoranges of the anchors and their position
+
+    Args:
+        anchors (dict): Dict with keys the anchor Id and value the UwbSensor
+
+    Returns:
+        np.array: The estimated position
+    """
     # Extract anchor positions and ranges
     anchor_pos, ranges, N = extract_anchor_data(anchors)
 
@@ -34,21 +60,34 @@ def ls_trilateration(anchors):
 
 
 def gauss_newton_trilateration(old_pos, anchors):
+    """
+    Perform a Gauss-Newton iteration to estimate the position of the sensor
+      from the pseudoranges of the anchors and their position
+
+    Args:
+        old_pos (np.array): The estimated position of the last iteration
+        anchors (dict): Dict with keys the anchor Id and value the UwbSensor
+
+    Returns:
+        np.array: The estimated position
+    """
+
     # Extract anchor positions and ranges
-    anchor_pos, ranges, N = extract_anchor_data(anchors)
+    anchor_pos, ranges, _ = extract_anchor_data(anchors)
 
     # Calculate Jacobian matrix
     den = ((old_pos[0] - anchor_pos[:, 0])**2 + (old_pos[1] -
-           anchor_pos[:, 1])**2 + (old_pos[2] - anchor_pos[:, 2])**2)**0.5
-
+                                                 anchor_pos[:, 1])**2 + (old_pos[2] - anchor_pos[:, 2])**2)**0.5
     num = np.transpose(old_pos) - anchor_pos
     J = np.divide(num, np.expand_dims(den, 1))
 
+    # Calculate the pseudoinverse and the residues
     pinvJ = np.linalg.pinv(-J)
     residue = ranges - ((old_pos[0]-anchor_pos[:, 0])**2 +
                         (old_pos[1]-anchor_pos[:, 1])**2 +
                         (old_pos[2]-anchor_pos[:, 2])**2)**0.5
 
+    # LS solution
     new_pos = old_pos - (pinvJ).dot(residue)
 
     return new_pos
