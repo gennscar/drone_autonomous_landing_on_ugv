65e9410bad9c73c8c103e8869b0741992f361373
Cosimo Conte <cosimocon@yahoo.it>
2021-05-11 15:31:20 +0200

    Formatting

--- src/control/control/drone_controller.py
+++ src/control/control/drone_controller.py
@@ -18,13 +18,13 @@ from rclpy.node import Node
 import functions
 import numpy as np
 
-from  px4_msgs.msg import OffboardControlMode
-from  px4_msgs.msg import TrajectorySetpoint
-from  px4_msgs.msg import Timesync
-from  px4_msgs.msg import VehicleCommand
-from  px4_msgs.msg import VehicleControlMode
-from  px4_msgs.msg import VehicleLocalPosition
-from  px4_msgs.msg import VehicleStatus
+from px4_msgs.msg import OffboardControlMode
+from px4_msgs.msg import TrajectorySetpoint
+from px4_msgs.msg import Timesync
+from px4_msgs.msg import VehicleCommand
+from px4_msgs.msg import VehicleControlMode
+from px4_msgs.msg import VehicleLocalPosition
+from px4_msgs.msg import VehicleStatus
 from nav_msgs.msg import Odometry
 from custom_interfaces.srv import ControlMode
 
@@ -36,15 +36,16 @@ INT_MAX = 250.0
 VMAX = float('inf')
 VMIN = - VMAX
 
+
 class OffboardControl(Node):
     def __init__(self):
         super().__init__("offboard_control")
 
         # Initialization to 0 of all parameters
-        self.int_e = np.array([0,0])
-        self.e_dot = np.array([0,0])
-        self.e_old = np.array([0,0])
-        self.e = np.array([0,0])
+        self.int_e = np.array([0, 0])
+        self.e_dot = np.array([0, 0])
+        self.e_old = np.array([0, 0])
+        self.e = np.array([0, 0])
         self.ARMING_STATE = 0
         self.LANDING_STATE = 0
         self.DESCENDING_STATE = 0
@@ -61,18 +62,26 @@ class OffboardControl(Node):
         self.control_mode = []
 
         # Publishers
-        self.offboard_control_mode_publisher_=self.create_publisher(OffboardControlMode,"OffboardControlMode_PubSubTopic",3)
-        self.trajectory_setpoint_publisher_=self.create_publisher(TrajectorySetpoint,"TrajectorySetpoint_PubSubTopic",3)
-        self.vehicle_command_publisher_=self.create_publisher(VehicleCommand,"VehicleCommand_PubSubTopic",3)
-        
+        self.offboard_control_mode_publisher_ = self.create_publisher(
+            OffboardControlMode, "OffboardControlMode_PubSubTopic", 3)
+        self.trajectory_setpoint_publisher_ = self.create_publisher(
+            TrajectorySetpoint, "TrajectorySetpoint_PubSubTopic", 3)
+        self.vehicle_command_publisher_ = self.create_publisher(
+            VehicleCommand, "VehicleCommand_PubSubTopic", 3)
+
         # Subscribers
-        self.drone_position_subscriber = self.create_subscription(VehicleLocalPosition,"VehicleLocalPosition_PubSubTopic",self.callback_local_position,3)
-        self.drone_status_subscriber = self.create_subscription(VehicleStatus,"VehicleStatus_PubSubTopic",self.callback_drone_status,3)
-        self.target_position_subscriber = self.create_subscription(Odometry,"/chassis/odom",self.callback_target_position,3)
-        self.timesync_sub_=self.create_subscription(Timesync,"Timesync_PubSubTopic",self.callback_timesync,3)
-        
+        self.drone_position_subscriber = self.create_subscription(
+            VehicleLocalPosition, "VehicleLocalPosition_PubSubTopic", self.callback_local_position, 3)
+        self.drone_status_subscriber = self.create_subscription(
+            VehicleStatus, "VehicleStatus_PubSubTopic", self.callback_drone_status, 3)
+        self.target_position_subscriber = self.create_subscription(
+            Odometry, "/chassis/odom", self.callback_target_position, 3)
+        self.timesync_sub_ = self.create_subscription(
+            Timesync, "Timesync_PubSubTopic", self.callback_timesync, 3)
+
         # Services
-        self.control_mode = self.create_service(ControlMode, "control_mode", self.callback_control_mode)
+        self.control_mode = self.create_service(
+            ControlMode, "control_mode", self.callback_control_mode)
 
         # Control loop timer
         self.timer = self.create_timer(0.1, self.timer_callback)
@@ -80,7 +89,7 @@ class OffboardControl(Node):
     def callback_timesync(self, msg):
         self.timestamp = msg.timestamp
 
-    def callback_local_position(self,msg):
+    def callback_local_position(self, msg):
         self.x = msg.x
         self.y = msg.y
         self.z = msg.z
@@ -102,14 +111,15 @@ class OffboardControl(Node):
             self.offboard_setpoint_counter_ += 1
         elif (-self.z) < 0.5:
             self.restart_drone()
-        
 
     def callback_drone_status(self, msg):
         self.ARMING_STATE = msg.arming_state
-    
-    def callback_target_position(self,msg):
-        self.target_global_pos = [msg.pose.pose.position.x, msg.pose.pose.position.y]
-        self.target_local_pos = [self.target_global_pos[1]-1, self.target_global_pos[0]-1]
+
+    def callback_target_position(self, msg):
+        self.target_global_pos = [
+            msg.pose.pose.position.x, msg.pose.pose.position.y]
+        self.target_local_pos = [
+            self.target_global_pos[1]-1, self.target_global_pos[0]-1]
 
     def callback_control_mode(self, request, response):
         if request.control_mode == "takeoff_mode":
@@ -181,84 +191,89 @@ class OffboardControl(Node):
             self.control_mode = 1
         else:
             msg = self.takeoff_mode(msg)
-        
+
         self.trajectory_setpoint_publisher_.publish(msg)
-        
 
-    def land_on_target_mode(self,msg):
-            msg.x = float("NaN")
-            msg.y = float("NaN")
+    def land_on_target_mode(self, msg):
+        msg.x = float("NaN")
+        msg.y = float("NaN")
 
-            self.e = np.array([self.x - self.target_local_pos[0], self.y - self.target_local_pos[1]])
-            [msg.vx, msg.vy], self.int_e, self.e_dot, self.e_old = functions.PID(KP, KI, KD, self.e, self.e_old, self.int_e, VMAX, VMIN, INT_MAX)
-            self.norm_e = np.linalg.norm(self.e, ord=2)
-            self.norm_e_dot = np.linalg.norm(self.e_dot, ord=2)
-            #self.get_logger().info(f"""int: {self.int_e}""")
-            """
+        self.e = np.array([self.x - self.target_local_pos[0],
+                          self.y - self.target_local_pos[1]])
+        [msg.vx, msg.vy], self.int_e, self.e_dot, self.e_old = functions.PID(
+            KP, KI, KD, self.e, self.e_old, self.int_e, VMAX, VMIN, INT_MAX)
+        self.norm_e = np.linalg.norm(self.e, ord=2)
+        self.norm_e_dot = np.linalg.norm(self.e_dot, ord=2)
+        #self.get_logger().info(f"""int: {self.int_e}""")
+        """
             if ((self.norm_e) < 0.1) and ((self.norm_e_dot) < 0.2) and (-self.z < 0.9):
                 self.LANDING_STATE = 1
                 self.get_logger().info("Landing..")
                 self.publish_vehicle_command(21, 0.0, 0.0)"""
-            if ((self.norm_e) < 0.1) and ((self.norm_e_dot) < 0.2) and self.LANDING_STATE == 0:
-                self.DESCENDING_STATE = 1
-                self.get_logger().info("Descending on target..")
-                msg.z = float("NaN")
-                msg.vz = 0.2
-            elif self.LANDING_STATE == 0 and self.DESCENDING_STATE == 0:
-                self.get_logger().info("Following target..")
-                msg.z = - 3.0
-                msg.vz = float("NaN")
-            elif self.LANDING_STATE == 0:
-                self.get_logger().info("Stopped descending..")
-                msg.z = - 1.5
-                msg.vz = - 0.1
-            """
-            if self.ARMING_STATE == 1 and self.LANDING_STATE == 1:
-                self.get_logger().info(f"Landed, with ex:{self.e[0]}, ey:{self.e[1]}")
-                rclpy.shutdown()"""
-            return msg
-
-    def target_follower_mode(self,msg):
-            msg.x = float("NaN")
-            msg.y = float("NaN")
-
-            self.e = np.array([self.x - self.target_local_pos[0], self.y - self.target_local_pos[1]])
-            [msg.vx, msg.vy], self.int_e, self.e_dot, self.e_old = functions.PID(KP, KI, KD, self.e, self.e_old, self.int_e, VMAX, VMIN, INT_MAX)
-            self.norm_e = np.linalg.norm(self.e, ord=2)
-            self.norm_e_dot = np.linalg.norm(self.e_dot, ord=2)
-
+        if ((self.norm_e) < 0.1) and ((self.norm_e_dot) < 0.2) and self.LANDING_STATE == 0:
+            self.DESCENDING_STATE = 1
+            self.get_logger().info("Descending on target..")
+            msg.z = float("NaN")
+            msg.vz = 0.2
+        elif self.LANDING_STATE == 0 and self.DESCENDING_STATE == 0:
             self.get_logger().info("Following target..")
             msg.z = - 3.0
             msg.vz = float("NaN")
-            return msg
+        elif self.LANDING_STATE == 0:
+            self.get_logger().info("Stopped descending..")
+            msg.z = - 1.5
+            msg.vz = - 0.1
+        """
+            if self.ARMING_STATE == 1 and self.LANDING_STATE == 1:
+                self.get_logger().info(f"Landed, with ex:{self.e[0]}, ey:{self.e[1]}")
+                rclpy.shutdown()"""
+        return msg
 
-    def setpoint_mode(self,msg):
-            msg.x = self.setpoint[0]
-            msg.y = self.setpoint[1]
-            msg.z = self.setpoint[2]
-            self.get_logger().info("Reaching setpoint..")
-            return msg
+    def target_follower_mode(self, msg):
+        msg.x = float("NaN")
+        msg.y = float("NaN")
 
+        self.e = np.array([self.x - self.target_local_pos[0],
+                          self.y - self.target_local_pos[1]])
+        [msg.vx, msg.vy], self.int_e, self.e_dot, self.e_old = functions.PID(
+            KP, KI, KD, self.e, self.e_old, self.int_e, VMAX, VMIN, INT_MAX)
+        self.norm_e = np.linalg.norm(self.e, ord=2)
+        self.norm_e_dot = np.linalg.norm(self.e_dot, ord=2)
 
-    def takeoff_mode(self,msg):
-            self.get_logger().info("Takeoff..")
-            msg.x = float("NaN")
-            msg.y = float("NaN")
-            msg.z = -3.0
-            return msg
+        self.get_logger().info("Following target..")
+        msg.z = - 3.0
+        msg.vz = float("NaN")
+        return msg
 
-    def landing_mode(self,msg):
-            self.get_logger().info("Landing..")
-            self.publish_vehicle_command(21, 0.0, 0.0)
-            return msg
+    def setpoint_mode(self, msg):
+        msg.x = self.setpoint[0]
+        msg.y = self.setpoint[1]
+        msg.z = self.setpoint[2]
+        self.get_logger().info("Reaching setpoint..")
+        return msg
+
+    def takeoff_mode(self, msg):
+        self.get_logger().info("Takeoff..")
+        msg.x = float("NaN")
+        msg.y = float("NaN")
+        msg.z = -3.0
+        return msg
+
+    def landing_mode(self, msg):
+        self.get_logger().info("Landing..")
+        self.publish_vehicle_command(21, 0.0, 0.0)
+        return msg
 
     def restart_drone(self):
-            self.offboard_setpoint_counter_=0
+        self.offboard_setpoint_counter_ = 0
 
-def main(args = None):
-    rclpy.init(args = args)
+
+def main(args=None):
+    rclpy.init(args=args)
     node = OffboardControl()
     rclpy.spin(node)
     rclpy.shutdown()
+
+
 if __name__ == "main":
     main()
--- src/estimation/estimation/uwb_vehicle.py
+++ src/estimation/estimation/uwb_vehicle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+import functions
 import rclpy
 import numpy as np
 from rclpy.node import Node
-from  px4_msgs.msg import VehicleLocalPosition
+from px4_msgs.msg import VehicleLocalPosition
 from nav_msgs.msg import Odometry
 from scipy.spatial.transform import Rotation as R
 from geometry_msgs.msg import Pose
@@ -14,12 +15,12 @@ from random import random
 # seed random number generator
 seed(1)
 
-import functions
 
 STD_TRILATERATION = False
 
 
-chassis_wrt_anchor0 = np.array([0.94859,0.44998,-0.32457])
+chassis_wrt_anchor0 = np.array([0.94859, 0.44998, -0.32457])
+
 
 class UwbPubSub(Node):
     def __init__(self):
@@ -36,17 +37,24 @@ class UwbPubSub(Node):
         self.anchor_1_orientation = [0.0, 0.0, 0.0, 1.0]
         self.anchor_2_orientation = [0.0, 0.0, 0.0, 1.0]
         self.anchor_3_orientation = [0.0, 0.0, 0.0, 1.0]
-        self.y = [random(),random(),random()]
+        self.y = [random(), random(), random()]
 
-        self.drone_position_subscriber = self.create_subscription(VehicleLocalPosition,"VehicleLocalPosition_PubSubTopic",self.callback_drone_position,10)
-        self.anchor_0_subscriber = self.create_subscription(Odometry,"/anchor_0/odom",self.callback_anchor_0,10)
-        self.anchor_1_subscriber = self.create_subscription(Odometry,"/anchor_1/odom",self.callback_anchor_1,10)
-        self.anchor_2_subscriber = self.create_subscription(Odometry,"/anchor_2/odom",self.callback_anchor_2,10)
-        self.anchor_3_subscriber = self.create_subscription(Odometry,"/anchor_3/odom",self.callback_anchor_3,10)
-        self.chassis_subscriber = self.create_subscription(Odometry,"/chassis/odom",self.callback_chassis,10)
+        self.drone_position_subscriber = self.create_subscription(
+            VehicleLocalPosition, "VehicleLocalPosition_PubSubTopic", self.callback_drone_position, 10)
+        self.anchor_0_subscriber = self.create_subscription(
+            Odometry, "/anchor_0/odom", self.callback_anchor_0, 10)
+        self.anchor_1_subscriber = self.create_subscription(
+            Odometry, "/anchor_1/odom", self.callback_anchor_1, 10)
+        self.anchor_2_subscriber = self.create_subscription(
+            Odometry, "/anchor_2/odom", self.callback_anchor_2, 10)
+        self.anchor_3_subscriber = self.create_subscription(
+            Odometry, "/anchor_3/odom", self.callback_anchor_3, 10)
+        self.chassis_subscriber = self.create_subscription(
+            Odometry, "/chassis/odom", self.callback_chassis, 10)
 
-        self.target_coordinates_publisher = self.create_publisher(Pose,"target_coordinates",10)
-        
+        self.target_coordinates_publisher = self.create_publisher(
+            Pose, "target_coordinates", 10)
+
         self.get_logger().info("uwb pub sub has started")
         self.timer = self.create_timer(0.1, self.timer_callback)
 
@@ -56,21 +64,19 @@ class UwbPubSub(Node):
         self.drone_local_vel = [msg.vx, msg.vy, msg.vz]
         self.drone_global_vel = [msg.vy, msg.vx, -msg.vz]
 
-
     def callback_chassis(self, msg):
         self.chassis_pos = [msg.pose.pose.position.x,
-                             msg.pose.pose.position.y,
-                             msg.pose.pose.position.z]
+                            msg.pose.pose.position.y,
+                            msg.pose.pose.position.z]
 
     def callback_anchor_0(self, msg):
         self.anchor_0_pos = [msg.pose.pose.position.x,
                              msg.pose.pose.position.y,
                              msg.pose.pose.position.z]
         self.anchor_0_orientation = [msg.pose.pose.orientation.x,
-                             msg.pose.pose.orientation.y,
-                             msg.pose.pose.orientation.z,
-                             msg.pose.pose.orientation.w]
-
+                                     msg.pose.pose.orientation.y,
+                                     msg.pose.pose.orientation.z,
+                                     msg.pose.pose.orientation.w]
 
     def callback_anchor_1(self, msg):
         self.anchor_1_pos = [msg.pose.pose.position.x,
@@ -102,52 +108,53 @@ class UwbPubSub(Node):
         self.r2 = np.subtract(self.anchor_2_pos, self.drone_global_pos)
         self.r3 = np.subtract(self.anchor_3_pos, self.drone_global_pos)
 
-        self.d0 = np.linalg.norm(self.r0, ord = 2)
-        self.d1 = np.linalg.norm(self.r1, ord = 2)
-        self.d2 = np.linalg.norm(self.r2, ord = 2)
-        self.d3 = np.linalg.norm(self.r3, ord = 2)
+        self.d0 = np.linalg.norm(self.r0, ord=2)
+        self.d1 = np.linalg.norm(self.r1, ord=2)
+        self.d2 = np.linalg.norm(self.r2, ord=2)
+        self.d3 = np.linalg.norm(self.r3, ord=2)
 
         self.get_logger().info(f"{self.d0},{self.d1},{self.d2},{self.d3}")
 
         if STD_TRILATERATION == True:
-            self.y = standard_trilateration.trilateration(self.d0, self.d1, self.d2, self.d3)
+            self.y = standard_trilateration.trilateration(
+                self.d0, self.d1, self.d2, self.d3)
         else:
-            self.y = gauss_newton_trilateration.trilateration(self.y, self.d0, self.d1, self.d2, self.d3)
-
+            self.y = gauss_newton_trilateration.trilateration(
+                self.y, self.d0, self.d1, self.d2, self.d3)
 
         self.rel_pos_anchor0 = self.y[1:]
         self.rot_world_to_anchor0 = R.from_quat(self.anchor_0_orientation)
 
-        self.rel_pos_chassis = np.subtract(self.rel_pos_anchor0, chassis_wrt_anchor0)
-        self.rel_pos_world_frame = - self.rot_world_to_anchor0.apply(self.rel_pos_chassis)
+        self.rel_pos_chassis = np.subtract(
+            self.rel_pos_anchor0, chassis_wrt_anchor0)
+        self.rel_pos_world_frame = - \
+            self.rot_world_to_anchor0.apply(self.rel_pos_chassis)
         self.get_logger().info(f"computed pos: {self.rel_pos_world_frame}")
 
-        
         self.true_pos = np.subtract(self.chassis_pos, self.drone_global_pos)
         self.get_logger().info(f"true pos: {self.true_pos}")
 
-        self.err_vec = np.subtract(self.rel_pos_world_frame , self.true_pos)
-        self.err = np.linalg.norm(self.err_vec, ord = 2)
+        self.err_vec = np.subtract(self.rel_pos_world_frame, self.true_pos)
+        self.err = np.linalg.norm(self.err_vec, ord=2)
 
-        #self.send_target_coordinates()
+        # self.send_target_coordinates()
 
-        #self.get_logger().info(f"""
-        #trilateration:{self.rel_pos_world_frame}
-        #true: {self.chassis_pos}""")
-        #self.get_logger().info(f"{self.err_vec}")
-        #self.get_logger().info(f"{self.rel_pos_world_frame}")
+        # self.get_logger().info(f"""
+        # trilateration:{self.rel_pos_world_frame}
+        # true: {self.chassis_pos}""")
+        # self.get_logger().info(f"{self.err_vec}")
+        # self.get_logger().info(f"{self.rel_pos_world_frame}")
 
-        #self.get_logger().info(f"{self.err}")
-
-
+        # self.get_logger().info(f"{self.err}")
 
 
-def main(args = None):
+def main(args=None):
 
-    rclpy.init(args = args)
+    rclpy.init(args=args)
     node = UwbPubSub()
     rclpy.spin(node)
     rclpy.shutdown()
+
 
 if __name__ == "main":
     main()
