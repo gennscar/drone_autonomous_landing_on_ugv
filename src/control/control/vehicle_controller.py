885fb784fdc72beab3b140d7bfedf2d1bd69f0b9
Cosimo Conte <cosimocon@yahoo.it>
2021-05-11 15:56:24 +0200

    Better formatting

--- src/control/control/vehicle_controller.py
+++ src/control/control/vehicle_controller.py
@@ -13,28 +13,28 @@ class VehicleController(Node):
         super().__init__("vehicle_controller")
 
         self.waypoint = 0
-        self.waypoint_list = [[20,0],[-20,0]]
-        self.vehicle_pos = [0,0]
+        self.waypoint_list = [[20, 0], [-20, 0]]
+        self.vehicle_pos = [0, 0]
         self.curr_waypoint = self.waypoint_list[self.waypoint]
 
-        self.vehicle_controller_publisher=self.create_publisher(Twist,"/rover_pic4ser/cmd_vel",10)
-        self.vehicle_state_subscriber=self.create_subscription(Odometry,"/rover_pic4ser/odom",self.vehicle_state_callback,10)
+        self.vehicle_controller_publisher = self.create_publisher(
+            Twist, "/rover_pic4ser/cmd_vel", 10)
+        self.vehicle_state_subscriber = self.create_subscription(
+            Odometry, "/rover_pic4ser/odom", self.vehicle_state_callback, 10)
 
         self.timer = self.create_timer(0.1, self.timer_callback)
 
     def timer_callback(self):
         self.e = np.subtract(self.curr_waypoint, self.vehicle_pos)
         self.norm_e = np.linalg.norm(self.e, ord=2)
-            
-        if  self.norm_e < 1:
+
+        if self.norm_e < 1:
             self.waypoint += 1
             if self.waypoint == 2:
                 self.waypoint = 0
             self.curr_waypoint = self.waypoint_list[self.waypoint]
 
-
         self.publish_vehicle_command()
-
 
     def publish_vehicle_command(self):
         msg = Twist()
@@ -45,15 +45,17 @@ class VehicleController(Node):
 
         self.vehicle_controller_publisher.publish(msg)
 
-    def vehicle_state_callback(self,msg):
+    def vehicle_state_callback(self, msg):
         self.vehicle_pos = [msg.pose.pose.position.x, msg.pose.pose.position.y]
 
-def main(args = None):
 
-    rclpy.init(args = args)
+def main(args=None):
+
+    rclpy.init(args=args)
     node = VehicleController()
     rclpy.spin(node)
     rclpy.shutdown()
+
 
 if __name__ == "main":
     main()
--- src/functions/functions/control_algorithms.py
+++ src/functions/functions/control_algorithms.py
@@ -2,20 +2,21 @@ import numpy as np
 import scipy.linalg
 from scipy.interpolate import interp1d
 
+
 def PID(kp, ki, kd, e, e_old, int_e, u_max, u_min, int_max):
 
     int_e = int_e + e
     e_dot = (e - e_old)/0.1
     e_old = e
     uk = - np.multiply(kp, e) - np.multiply(ki, int_e) - np.multiply(kd, e_dot)
-    
+
     uk = np.clip(uk, u_min, u_max)
     int_e = np.clip(int_e, - int_max, int_max)
-    
+
     return uk, int_e, e_dot, e_old
 
-def DLQR_optimizer(A, B, Q, R):
 
+def DLQR_optimizer(A, B, Q, R):
     """
     Solve the discrete time lqr controller.
     x[k+1] = A x[k] + B u[k]
@@ -27,14 +28,15 @@ def DLQR_optimizer(A, B, Q, R):
     K = np.matrix(scipy.linalg.inv(B.T*P*B+R)*(B.T*P*A))
     return K
 
+
 def DLQR(K, xk, u_max, u_min):
 
     uk = K*xk
     uk = np.clip(uk, u_min, u_max)
 
-
     return uk
-    
+
+
 def gain_scheduling_optimizer(list_states, list_kp, list_ki, list_kd):
 
     kp_interpolated = interp1d(list_states, list_kp, kind='linear')
@@ -42,7 +44,3 @@ def gain_scheduling_optimizer(list_states, list_kp, list_ki, list_kd):
     kd_interpolated = interp1d(list_states, list_kd, kind='linear')
 
     return kp_interpolated, ki_interpolated, kd_interpolated
-
-
-
-
