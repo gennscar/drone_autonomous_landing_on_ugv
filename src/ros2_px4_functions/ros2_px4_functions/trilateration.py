87de75489dce54d274e5c43c083715092f514195
Cosimo Conte <cosimocon@yahoo.it>
2021-05-31 15:44:11 +0200

    Trilateration does not perform extraction anymore

--- src/ros2_px4_functions/ros2_px4_functions/trilateration.py
+++ src/ros2_px4_functions/ros2_px4_functions/trilateration.py
@@ -1,52 +1,22 @@
 import numpy as np
 
 
-def extract_anchor_data(anchors):
-    """Extract all the data from a dict of anchors
-
-    Args:
-        anchors (dict): Dict with keys the anchor Id and value the UwbSensor
-          message sent by that anchor, in this way only the last message is
-          saved
-
-    Returns:
-        anchor_pos (np.array): Matrix containing for each row the 3 coordinates
-          of one anchor
-
-        ranges (np.array): Array containing all the pseudoranges
-
-        N (int): Number of anchors
-    """
-
-    N = len(anchors)
-
-    # Extract anchor positions and ranges
-    i = 0
-    anchor_pos = np.empty((N, 3))
-    ranges = np.empty(N)
-
-    for _, data in anchors.items():
-        anchor_pos[i, :] = np.array(
-            [data.anchor_pos.x, data.anchor_pos.y, data.anchor_pos.z])
-        ranges[i] = data.range
-        i = i+1
-
-    return anchor_pos, ranges, N
-
-
-def ls_trilateration(anchors):
+def ls_trilateration(anchor_pos, ranges, N):
     """
     Solve the Least-Square problem to estimate the position of the sensor
       from the pseudoranges of the anchors and their position
 
     Args:
-        anchors (dict): Dict with keys the anchor Id and value the UwbSensor
+        anchor_pos (np.array): Matrix containing for each row the 3 coordinates 
+        of one anchor
+
+        ranges (np.array): Array containing all the pseudoranges
+
+        N (int): Number of anchors
 
     Returns:
         np.array: The estimated position
     """
-    # Extract anchor positions and ranges
-    anchor_pos, ranges, N = extract_anchor_data(anchors)
 
     # Calculate b, A and its pseudoinverse
     b = ranges**2 - anchor_pos[:, 0]**2 - \
@@ -59,21 +29,24 @@ def ls_trilateration(anchors):
     return y[1:]
 
 
-def gauss_newton_trilateration(old_pos, anchors):
+def gauss_newton_trilateration(old_pos, anchor_pos, ranges):
     """
     Perform a Gauss-Newton iteration to estimate the position of the sensor
       from the pseudoranges of the anchors and their position
 
     Args:
         old_pos (np.array): The estimated position of the last iteration
-        anchors (dict): Dict with keys the anchor Id and value the UwbSensor
+
+        anchor_pos (np.array): Matrix containing for each row the 3 coordinates 
+        of one anchor
+
+        ranges (np.array): Array containing all the pseudoranges
+
+        N (int): Number of anchors
 
     Returns:
         np.array: The estimated position
     """
-
-    # Extract anchor positions and ranges
-    anchor_pos, ranges, _ = extract_anchor_data(anchors)
 
     # Calculate Jacobian matrix
     den = ((old_pos[0] - anchor_pos[:, 0])**2 +
