40581829c33f8336fee778b551efbdd1496da823
Cosimo Conte <cosimocon@yahoo.it>
2021-10-27 18:47:48 +0200

    Better simulation error detector

--- src/ros2_px4_testing/ros2_px4_testing/odometry_error.py
+++ src/ros2_px4_testing/ros2_px4_testing/odometry_error.py
@@ -4,45 +4,35 @@ import rclpy
 from rclpy.node import Node
 import numpy as np
 
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from nav_msgs.msg import Odometry
+from px4_msgs.msg import VehicleLocalPosition
+from px4_msgs.msg import VehicleLocalPositionGroundtruth
 from ros2_px4_interfaces.msg import Error
 
 
-class PositioningError(Node):
+class OdometryError(Node):
     """Node to estimate the position error of estimators"""
 
     def __init__(self):
-        super().__init__("positioning_error")
+        super().__init__("odometry_error")
 
         self.sensor_real_pos_ = []
-        self.estimator_topics_ = {}
 
         # Sensor subscriber to the real position
         self.sensor_real_subscriber_ = self.create_subscription(
-            Odometry, "/uwb_sensor_iris/odom", self.callback_real_subscriber, 10)
+            VehicleLocalPositionGroundtruth, "VehicleLocalPositionGroundtruth_PubSubTopic", self.callback_real_subscriber, 10)
 
-        # Timer to check the creation of new estimators
-        self.timer_ = self.create_timer(1, self.timer_callback)
+        # Sensor subscriber to the estimated position
+        self.sensor_est_subscriber_ = self.create_subscription(
+            VehicleLocalPosition, "VehicleLocalPosition_PubSubTopic", self.callback_est_subscriber, 10)
+
+        # Error
+        self.sensor_err_publisher_ = self.create_publisher(
+            Error, "LocalPositionError", 10
+        )
 
         self.get_logger().info(f"""Node has started""")
 
-    def timer_callback(self):
-        """Timer to check the creation of new estimators"""
-
-        for topic_name, _ in self.get_topic_names_and_types():
-            if("estimated_pos" in topic_name and topic_name not in self.estimator_topics_.keys()):
-                # Creation to a new subscriber and error publisher for the new estimator
-                self.create_subscription(
-                    PoseWithCovarianceStamped, topic_name, self.callback_sensor_subscriber, 10)
-                self.estimator_topics_[topic_name] = self.create_publisher(
-                    Error, topic_name.replace("estimated_pos", "") + "error", 10)
-
-                self.get_logger().info(f"""
-                                       Connected to: {topic_name}
-                                       """)
-
-    def callback_sensor_subscriber(self, msg):
+    def callback_est_subscriber(self, msg):
         """
         Retriving the estimated/sensor position and evaluating mean square error
 
@@ -52,15 +42,13 @@ class PositioningError(Node):
 
         # Retriving the estimated position
         sensor_est_pos = np.array([
-            msg.pose.pose.position.x,
-            msg.pose.pose.position.y,
-            msg.pose.pose.position.z
+            msg.x, msg.y, msg.z, msg.vx, msg.vy, msg.vz,
         ])
 
         # Filling error message
         error = Error()
         error.header.stamp = self.get_clock().now().to_msg()
-        error.header.frame_id = msg.header.frame_id
+        error.header.frame_id = "This nuts"
 
         if(self.sensor_real_pos_ != []):
             diff = sensor_est_pos - self.sensor_real_pos_
@@ -68,6 +56,7 @@ class PositioningError(Node):
             error.rmse = np.linalg.norm(diff)
             error.mse = error.rmse**2
 
+            """
             P = np.array([
                 [
                     msg.pose.covariance[0], msg.pose.covariance[1], msg.pose.covariance[2]
@@ -87,10 +76,10 @@ class PositioningError(Node):
             else:
                 error.nees = np.matmul(
                     np.matmul(np.transpose(diff), Pinv), diff)
+            """
 
         # Sending error message only if the estimator is valid
-        if(msg.header.frame_id in self.estimator_topics_.keys()):
-            self.estimator_topics_[msg.header.frame_id].publish(error)
+        self.sensor_err_publisher_.publish(error)
 
     def callback_real_subscriber(self, msg):
         """
@@ -100,15 +89,13 @@ class PositioningError(Node):
             msg (Odometry): True position
         """
         self.sensor_real_pos_ = np.array([
-            msg.pose.pose.position.x,
-            msg.pose.pose.position.y,
-            msg.pose.pose.position.z
+            msg.x, msg.y, msg.z, msg.vx, msg.vy, msg.vz,
         ])
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = PositioningError()
+    node = OdometryError()
     rclpy.spin(node)
     rclpy.shutdown()
 
