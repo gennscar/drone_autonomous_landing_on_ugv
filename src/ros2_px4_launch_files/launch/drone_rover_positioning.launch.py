32cfa794f629473b7f5f9015400545b615376866
gennscar <gennscar97@gmail.com>
2021-11-02 12:04:27 +0100

    added predictive setpoint in simulation

--- src/ros2_px4_launch_files/launch/drone_rover_positioning.launch.py
+++ src/ros2_px4_launch_files/launch/drone_rover_positioning.launch.py
@@ -8,22 +8,21 @@ drone_name = "/drone"
 kf_params_pos = [
 
     [{'deltaT': 1e-1}, {'R_uwb': 1e-2}, {'R_px4': 5e-1}, {'R_range_sensor': 1e-2},
-    {'R_compass': 1e0}, {'Q_drone': 2e-4}, {'Q_rover': 5e-2}, 
+    {'R_compass': 1e0}, {'Q_drone': 5e-3}, {'Q_rover': 5e-3}, 
     {'Q_compass': 2e3}, {'Q_rover_z': 1e-8}, {'Q_drone_z': 1e-1}, 
     {'rng_sensor_fuse_radius': 0.40}, {'vehicle_namespace': drone_name}, {'uwb_estimator': "/LS_uwb_estimator/norot_pos"},
     {"yaw_subscriber_topic": yaw_topic_name}, {"enable_watchdog": True}],
 
-]
-
-kf_bias_params_pos = [
-
     [{'deltaT': 1e-1}, {'R_uwb': 1e-2}, {'R_px4': 5e-1}, {'R_range_sensor': 5e-2},
-    {'R_compass': 1e0}, {'R_apriltag': 1e-2}, {'Q_drone': 1e-4}, {'Q_rover': 3e-2}, 
-    {'Q_compass': 1e2}, {'Q_rover_z': 1e-8}, {'Q_drone_z': 1e-1}, {'Q_bias': 1e-5},
-    {'rng_sensor_fuse_radius': 0.40}, {'vehicle_namespace': drone_name}, {'uwb_estimator': "/LS_uwb_estimator/norot_pos"},
+    {'R_compass': 1e0}, {'Q_drone': 2e-4}, {'Q_rover': 5e-2}, {'Q_compass': 2e3},
+    {'Q_rover_z': 1e-8}, {'Q_drone_z': 1e-1}, {'rng_sensor_fuse_radius': 0.30},
+    {'vehicle_namespace': drone_name}, {'uwb_estimator': "/LS_uwb_estimator/norot_pos"},
     {"yaw_subscriber_topic": yaw_topic_name}, {"enable_watchdog": True}],
 
+
 ]
+
+
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -35,7 +34,7 @@ def generate_launch_description():
         name = "gazebo_yaw_estimator",
         parameters = [
             {"yaw_publisher_topic": yaw_topic_name},
-            {"yaw_offset": 15.0},
+            {"yaw_offset": 25.0},
             {"yaw_std_dev": 0.75}
         ]
     ))
@@ -58,14 +57,6 @@ def generate_launch_description():
             package='ros2_px4_estimation',
             executable='drone_rover_kf_pos_theta_sim',
             namespace='KF_pos_estimator_' + str(i),
-            parameters=param
-        ))
-
-    for i, param in enumerate(kf_bias_params_pos):
-        ld.add_entity(Node(
-            package='ros2_px4_estimation',
-            executable='drone_rover_kf_pos_theta_bias_sim',
-            namespace='KF_pos_estimator_bias_' + str(i),
             parameters=param
         ))
 
