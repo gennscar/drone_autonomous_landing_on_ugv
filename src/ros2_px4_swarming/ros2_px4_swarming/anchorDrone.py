9256327e53a246fc3f304d8c7a107908d3e598b5
MatteoCelada <s224618@studenti.polito.it>
2021-11-04 16:22:30 +0100

    Small modifications (pt. 2)

--- src/ros2_px4_swarming/ros2_px4_swarming/anchorDrone.py
+++ src/ros2_px4_swarming/ros2_px4_swarming/anchorDrone.py
@@ -552,7 +552,8 @@ class Drone(Node):
         # if self.authorizedForOffboard and (self.vehicleStatus.latest_disarming_reason == VehicleStatus.ARM_DISARM_REASON_RC_STICK or \
         #                                    self.vehicleStatus.latest_disarming_reason == VehicleStatus.ARM_DISARM_REASON_RC_SWITCH):
             self.authorizedForOffboard = False
-            self.publishDroneInfo("RC took control")
+            if self.vehicleStatus.nav_state != VehicleStatus.NAVIGATION_STATE_AUTO_LAND:
+                self.publishDroneInfo("RC took control")
             self.droneMode = "idle"
 
     def anchorReadyForTakeoffCallback(self, msg):
